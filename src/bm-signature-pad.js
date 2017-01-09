260421b892823011958c233e2ddce28828cfd250
Matt Carroll <matt.carroll139@gmail.com>
2017-01-09 15:06:09 +1100

    SPA-1 # Require ngModel for component

--- src/bm-signature-pad.js
+++ src/bm-signature-pad.js
@@ -13,58 +13,79 @@ function BmSignaturePadController ($scope, $element, $window, $log) {
   const vm = this
   const element = $element[0]
   const canvas = element.children[0]
+  let signaturePad
 
-  // Set canvas initial size to the fill parent element: <bm-signature-pad></bm-signature-pad>
+  // Set canvas initial size to fill parent element: <bm-signature-pad></bm-signature-pad>
   canvas.width = element.clientWidth
   canvas.height = element.clientHeight
 
+  vm.$onDestroy = () => {
+    if (signaturePad && angular.isFunction(signaturePad.off)) {
+      signaturePad.off()
+    }
+  }
   vm.$onInit = () => {
     const opts = vm.options || {}
+    const getSignature = () => {
+      if (!signaturePad || signaturePad.isEmpty()) {
+        return undefined
+      }
 
-    // Allow onBegin and onEnd to be set from the options attribute
-    // but override if specified from specific attributes
-    if (vm.beforeDraw) {
-      opts.onBegin = vm.beforeDraw
-    }
-    if (vm.afterDraw) {
-      opts.onEnd = vm.afterDraw
+      const args = [
+        vm.imageType ? vm.imageType() : undefined,
+        vm.imageEncoder ? vm.imageEncoder() : undefined
+      ]
+      if (vm.crop && vm.crop()) {
+        return signaturePad.toDataURLCropped(...args)
+      } else {
+        return signaturePad.toDataURL(...args)
+      }
     }
 
-    // Need to wrap the a few things in an $apply to ensure a digest cycle is started
+    // Need to wrap the a onBegin and onEnd in an $apply to ensure a digest cycle is started
     const wrapFunction = (fn) => {
       if (angular.isFunction(fn)) {
         return () => $scope.$apply(() => fn())
       }
     }
     opts.onBegin = wrapFunction(opts.onBegin)
-    opts.onEnd = wrapFunction(opts.onEnd)
-    opts.initValueCallback = wrapFunction(opts.initValueCallback)
+    const onEnd = opts.onEnd
+    opts.onEnd = wrapFunction(() => {
+      vm.ngModel.$setViewValue(getSignature())
+      if (angular.isFunction(onEnd)) {
+        onEnd()
+      }
+    })
 
-    const signaturePad = new SignaturePad(canvas, opts)
+    signaturePad = new SignaturePad(canvas, opts)
 
     // Functions that are made available to the parent component
     if (vm.clear) {
       vm.clear({
-        $fn: () => signaturePad.clear()
+        $fn: () => {
+          signaturePad.clear()
+          vm.ngModel.$setViewValue(undefined)
+        }
       })
     }
     if (vm.resize) {
       vm.resize({
-        $fn: () => signaturePad.resize(element.clientWidth, element.clientHeight, vm.scaleDown && vm.scaleDown())
-      })
-    }
-    if (vm.getSignature) {
-      vm.getSignature({
-        $fn: (...args) => {
-          if (signaturePad.isEmpty()) {
-            return undefined
-          } else if (vm.crop && vm.crop()) {
-            return signaturePad.toDataURLCropped(...args)
-          } else {
-            return signaturePad.toDataURL(...args)
-          }
+        $fn: () => {
+          signaturePad.resize(element.clientWidth, element.clientHeight, vm.scaleDown && vm.scaleDown())
+          vm.ngModel.$setViewValue(getSignature())
         }
       })
+    }
+
+    // Specify how the model is deemed empty
+    vm.ngModel.$isEmpty = () => signaturePad.isEmpty()
+    // Specify how UI should be updated
+    vm.ngModel.$render = () => {
+      if (vm.ngModel.$viewValue) {
+        signaturePad.fromDataURL(vm.ngModel.$viewValue)
+      } else {
+        signaturePad.clear()
+      }
     }
   }
 }
@@ -72,15 +93,17 @@ function BmSignaturePadController ($scope, $element, $window, $log) {
 const bmSignaturePadComponent = {
   template: '<canvas class="signature-pad">Your browser does not support the HTML5 canvas tag.</canvas>',
   controller: BmSignaturePadController,
+  require: {
+    ngModel: 'ngModel'
+  },
   bindings: {
     options: '<?',
-    beforeDraw: '&?',
-    afterDraw: '&?',
     crop: '&?',
+    imageType: '&?',
+    imageEncoder: '&?',
     scaleDown: '&?',
     clear: '&?',
-    resize: '&?',
-    getSignature: '&?'
+    resize: '&?'
   }
 }
 
